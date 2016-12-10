76d97296d4a15fbd702eef7c35d060eb265bf10c
JulioFig <JulioFigueroa@outlook.com>
2016-12-10 00:30:16 -0500

    Fixed server problems

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://witsport.github.io/bin/contact_me.php",
+                url: "bin/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
